826eeacb2b2a456e8effb4d3dca3c359aacb9ca6
James Gardner <crungmungus@gmail.com>
2013-10-11 16:48:08 +0100

    Converted indentation to spaces.

--- sp-list/app/scripts/app/routers/clinics.js
+++ sp-list/app/scripts/app/routers/clinics.js
@@ -1,16 +1,16 @@
 define(function (require) {
   var Presenter = require('presenters/ClinicPresenter'),
-  		View = require('views/ClinicListView'),
-  		Form = require('views/ClinicFormView'),
-  		Collection = require('collections/ClinicCollection');
+      View = require('views/ClinicListView'),
+      Form = require('views/ClinicFormView'),
+      Collection = require('collections/ClinicCollection');
 
   return Backbone.SubRoute.extend({
-		initialize: function () {
-			new Presenter({
-				view : new View(),
-				form : new Form(),
-				collection : new Collection()
-			});
-		}
-	});
+    initialize: function () {
+      new Presenter({
+        view : new View(),
+        form : new Form(),
+        collection : new Collection()
+      });
+    }
+  });
 });
